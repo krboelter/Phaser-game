4eaff00341ace2ed923b32782f79ccc279773f80
krboelter <krboelter@gmail.com>
2019-07-12 23:13:24 -0600

    Added some stuff

--- game.js
+++ game.js
@@ -2,6 +2,13 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y : 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -9,16 +16,37 @@ var config = {
     }
 };
 
+var sky;
+var player;
+var cursors;
+
 var game = new Phaser.Game(config);
 
 function preload() {
-
+    this.load.image('sky', 'Assets/Environment/sky.png');
+    this.load.image('player', 'Assets/Player/player.png');
 }
 
 function create() {
+    this.add.image(400, 300, 'sky');
+    
+    player = this.physics.add.sprite(400, 400, 'player');
 
+    player.setBounce(0.1);
+    player.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+    }
+    else {
+        player.setVelocityX(0);
+    }
 }
+
